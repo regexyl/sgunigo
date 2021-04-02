61e537c9f9ec525abc31ac912382b4dfc95f8580
rlyx-31 <regina.liuyixuan@gmail.com>
2021-04-03 02:24:16 +0800

    linked admin backend

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,34 +1,32 @@
-const { request } = require("express");
+const fetch = require('node-fetch')
 const express = require("express");
 const router = express.Router();
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
-const applications_uni_api = 'http://localhost:5001/'
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "9";
+
+const applications_uni_api = 'http://application:5001/application/'
 
 // @desc    View dashboard for university admins
 // @route   GET /admin/:university
 router.get("/:university", async (req, res) => {
     const university = req.params.university
-    request(applications_uni_api.concat(university), {json: true}, (error, res, body) => {
-      if (error) {
-        return console.log(error)
-      }
-      if (!error && res.statusCode == 200) {
-        const data = body
-        console.log(data)
-        return data
-      }
-    })
+    const settings = {
+        method: 'GET'
+    };
     try {
-      res.render("admin/index", {
-        layout: "admin",
-        university,
-        data
-      });
+        const fetchResponse = await fetch(applications_uni_api.concat(university), settings);
+        const applications = await fetchResponse.json();
+        res.render("admin/index", {
+          layout: "admin",
+          university,
+          applications
+        });
     } catch (err) {
       console.error(err)
       res.render('error/500')
     }
-});
+      
+})
 
 module.exports = router;
