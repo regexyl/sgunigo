c623925e8c5fdb0fd5de0b8aeb122dbb8fef706a
kellylikesjelly <70317445+kellylikesjelly@users.noreply.github.com>
2021-04-05 17:58:38 +0800

    change links for kong

---
+++ app/public/js/config.js
@@ -0,0 +1,14 @@
+const IP_ADDRESS='http://172.20.10.4.'
+
+// const IP_ADDRESS='http://localhost';
+
+const API_KEY_APPLICANT='AYuRJuTIMUUfqYAANsTGJlxX8YVkCwTT';
+
+const API_KEY_ADMIN='auo7YwJWqPspBa3YUn0jk5WSZNErdQFH';
+
+// module.exports = {
+//     IP_ADDRESS,
+//     API_KEY_APPLICANT,
+//     API_KEY_ADMIN
+//   };
+  
--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,11 +1,17 @@
 const fetch = require('node-fetch')
 const express = require("express");
 const router = express.Router();
+const dotenv = require("dotenv");
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
+// const constants = require("../middleware/config");
+
+dotenv.config({ path: `./config/${process.env.NODE_ENV}.env` });
+
+console.log(process.env.IP_ADDRESS)
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
-const applications_api = 'http://application:5001/application/'
+const applications_api = (constants.IP_ADDRESS).concat(':8000/application/');
 
 // @desc    View dashboard for university admins
 // @route   GET /admin/:university
@@ -15,7 +21,7 @@ router.get("/:university", async (req, res) => {
         method: 'GET'
     };
     try {
-        const fetchResponse = await fetch(applications_api.concat(university), settings);
+        const fetchResponse = await fetch(applications_api.concat(university).concat('?apikey=').concat(constants.API_KEY_ADMIN), settings);
         const applications = await fetchResponse.json();
         res.render("admin/index", {
           layout: "admin",
