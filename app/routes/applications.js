8cf27f21199f1a3cde3fde3b23e011a5992e3347
kellylikesjelly <70317445+kellylikesjelly@users.noreply.github.com>
2021-04-05 19:42:36 +0800

    sigh

--- app/routes/applications.js
+++ app/routes/applications.js
@@ -2,12 +2,12 @@ const fetch = require('node-fetch')
 const express = require("express");
 const router = express.Router();
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
-const Profile = require('../models/Profile')
+const Profile = require('../models/Profile');
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 const applications_api = 'http://application:5001/application/'
-
+const IP_ADDRESS='http://172.20.10.4.';
 // @desc    View dashboard of applications
 // @route   GET /applications/index
 router.get("/", ensureAuth, async (req, res) => {
